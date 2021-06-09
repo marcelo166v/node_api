d61d546695b3fc2e13830dbbf9bcbebbbd40b695
Marcelo Villani <marcelovillani166@hotmail.com>
2021-06-08 22:47:09 -0300

    Get de mascota en API express

--- Express/index.js
+++ Express/index.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+//const recursos = require('../Veterinario-FullStack/BackEnd/recursos/')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+// Ruta para mascotas
+app.get('/mascotas', (req, res) => {
+    const mascotas = [
+        {tipo:"perro", nombre:"firulais 1",dueno: "marcelo"},
+        {tipo:"perro", nombre:"firulais 2",dueno: "marcelo"},
+        {tipo:"perro", nombre:"firulais 3",dueno: "marcelo"},
+    ];
+    res.status(200).json(mascotas); // Codigo OK
 })
 
 app.listen(port, () => {
