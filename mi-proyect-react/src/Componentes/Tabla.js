a79b6dcfc78f0dc8b98834778eece1aeb8021a36
Marcelo Villani <marcelovillani166@hotmail.com>
2021-06-13 19:19:52 -0300

    Componente tabla react

---
+++ mi-proyect-react/src/Componentes/Tabla.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+
+function Tabla(){
+    return(
+        <table className="table">
+        <thead className="table-dark">
+            <tr>
+                <th>#</th>
+                <th>Tipo</th>
+                <th>Nombre</th>
+                <th>Dueño</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody id="bodyMascotas">
+    
+        </tbody>
+    
+    </table>  
+    );
+}
+
+export default Tabla;
